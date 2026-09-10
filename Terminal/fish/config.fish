465b1af71899b92936a9ba2cd20194e6a341fb4f
bento143asf <bentoproribeiro2@gmail.com>
2026-09-10 15:06:56 -0300

    Editing some confs

--- Terminal/fish/config.fish
+++ Terminal/fish/config.fish
@@ -1,9 +1,7 @@
 if status is-interactive
-
 fastfetch
 alias nixclean="sudo nix-collect-garbage -d"
 alias nixconf="sudo nano /etc/nixos/configuration.nix"
 alias nixrebuild "sudo nixos-rebuild switch"
 alias hyprconf="nano ~/.config/hypr/hyprland.lua"
-
 end
